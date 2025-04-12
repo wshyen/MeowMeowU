75bfc7056a843cfbc570e8112481aeebed40fb86
Shyen <limwanshyen@gmail.com>
2025-04-12 23:25:24 +0800

    Update coding

--- Website/__init__.py
+++ Website/__init__.py
@@ -4,4 +4,10 @@ def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = "Hello"
 
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, urlprefix="/")
+    app.register_blueprint(auth, urlprefix="/")
+
     return app
