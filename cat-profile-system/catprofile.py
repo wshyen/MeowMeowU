ec6af25f60d1127c7a7332b4debcd092e9734d93
Breann <breannleemy@gmail.com>
2025-04-13 16:45:59 +0800

    Added allowed image extensions for file uploads

--- cat-profile-system/catprofile.py
+++ cat-profile-system/catprofile.py
@@ -4,6 +4,7 @@ from flask import Flask
 app = Flask(__name__) 
 
 UPLOAD_FOLDER = 'static/uploads'
+ALLOWED_EXTENSIONS = {'png','jpg','jpeg'}
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 # Temporary storage for cat profiles
