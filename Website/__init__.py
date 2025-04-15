f29f057fcc28e56b19726bae27dd37a7418ec838
Shyen <limwanshyen@gmail.com>
2025-04-16 00:46:21 +0800

    Do database creation and foreign key relationships

--- Website/__init__.py
+++ Website/__init__.py
@@ -1,13 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path
 
-db = SQLAlchemy
+db = SQLAlchemy()
 DB_NAME = "datebase.db"
 
 def create_app():
     app = Flask(__name__)
-    app.config["SECRET_KEY"] = "Hello"
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SECRET_KEY'] = "Hello"
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{DB_NAME}"
     db.init_app(app)
 
     from .views import views
@@ -16,4 +17,12 @@ def create_app():
     app.register_blueprint(views, urlprefix="/")
     app.register_blueprint(auth, urlprefix="/")
 
+    from .models import User, Note #import this to make sure models.py file run before we initialize database
+
     return app
+
+def create_database(app):
+    if not path.exists("Website/" + DB_NAME):
+        db.create_all(app=app)
+        print("Created database!")
+
