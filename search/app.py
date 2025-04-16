772e6523aa5089886dd223a955cf37817771aa18
Yinnie <yinniesiew@gmail.com>
2025-04-17 00:25:28 +0800

    change color from option become checkbox

--- search/app.py
+++ search/app.py
@@ -15,12 +15,12 @@ def search():
 def cat_list():
     name = request.args.get("name", "").lower()
     gender = request.args.get("gender", "")
-    color_list = request.args.get("color[]")
+    color_list = request.args.getlist("color")
 
     filtered_cats = [
     cat for cat in cat_data
-        if (not name or name in cat["name"].lower()) and \
-           (not gender or gender == cat["gender"]) and \
+        if (not name or name in cat["name"].lower()) and
+           (not gender or gender == cat["gender"]) and 
            (not color_list or cat["color"] in color_list)
     ]
 
