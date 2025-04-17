b8851e3508d11a5ec9b4035b50b850498809cf88
Breann <breannleemy@gmail.com>
2025-04-17 23:23:41 +0800

    Added missing Flask imports for request handling

--- cat-profile-system/catprofile.py
+++ cat-profile-system/catprofile.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, flash
+from flask import Flask, render_template, request, redirect, url_for, flash
 
 app = Flask(__name__) 
 app.auth_key = 'auth_key'
