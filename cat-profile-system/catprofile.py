300cdf5b5940cdf6814a8522884ccd33c1ec5405
Breann <breannleemy@gmail.com>
2025-04-17 17:18:26 +0800

    Updated route to display all cat profiles

--- cat-profile-system/catprofile.py
+++ cat-profile-system/catprofile.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__) 
 app.auth_key = 'auth_key'
@@ -8,11 +8,15 @@ UPLOAD_FOLDER = 'static/uploads'
 ALLOWED_EXTENSIONS = {'png','jpg','jpeg'}
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-# Temporary storage for cat profiles
+#Temporary storage for cat profiles
 cat_profiles = []
 
+@app.route('/profiles')
+def view_profiles(): #View all cat profiles
+    return render_template('viewprofile.html', profiles=cat_profiles)
+
 if __name__ == '__main__':
-    # Ensure the upload folder exists
+    #Ensure the upload folder exists
     if not os.path.exists(UPLOAD_FOLDER):
         os.makedirs(UPLOAD_FOLDER)
     app.run(debug=True)
