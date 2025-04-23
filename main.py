ecb1e819ee8721df6898b70cb4d8b341aaac0cf6
Shyen <limwanshyen@gmail.com>
2025-04-23 16:20:25 +0800

    fixing error

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-from Website import create_app
+from Website import create_app #import create_app func from Websute module
 
-app = create_app()
+app = create_app() #apply the func to initialize the Flask application
 
-if __name__ == "__main__":
-    app.run(debug=True)
-
+if __name__ == "__main__": #the "__main__" makes sure this code runs only when u start the script urself
+    app.run(debug=True) #run flask application
