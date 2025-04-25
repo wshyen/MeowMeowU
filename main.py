167ed8cb433275e0f5741294f66ed7149b36fa94
Breann <breannleemy@gmail.com>
2025-04-25 16:58:13 +0800

    Removed duplicate files

--- main.py
+++
@@ -1,6 +0,0 @@
-from Website import create_app #import create_app func from Websute module
-
-app = create_app() #apply the func to initialize the Flask application
-
-if __name__ == "__main__": #the "__main__" makes sure this code runs only when u start the script urself
-    app.run(debug=True) #run flask application
