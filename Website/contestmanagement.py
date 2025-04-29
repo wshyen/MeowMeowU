b360a4171cacf946d34e1188032ef06983476f4c
Breann <breannleemy@gmail.com>
2025-04-29 23:59:46 +0800

    Added login route, blueprint, file upload setup and static folder for contests

--- Website/contestmanagement.py
+++ Website/contestmanagement.py
@@ -1,20 +1,46 @@
-from flask import Flask, render_template, request, session, redirect, url_for, jsonify, flash
+import os
+import sqlite3
+from flask import Flask, Blueprint, render_template, request, session, redirect, url_for, jsonify, flash
+from flask_login import current_user 
+from werkzeug.utils import secure_filename
 from datetime import datetime
 
-app = Flask(__name__)
-app.secret_key = 'your_secret_key'  
+app = Flask(__name__, static_folder='static')
+app.secret_key = 'your_secret_key'
+contestmanagement_bp = Blueprint('contestmanagement', __name__, template_folder='templates', static_folder='static')
 
-@app.route("/")
+UPLOAD_FOLDER = 'static/uploads'
+ALLOWED_EXTENSIONS = {'png','jpg','jpeg'}
+app.config['UPLOAD_FOLDER'] = 'static/contest' #Folder to store uploaded files
+app.config['SECRET_KEY'] = 'your_secret_key'
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        
+        # Assuming user validation is correct
+        session['user'] = username  # Set the session when the user is logged in
+        return redirect(url_for('contestmanagement.contest'))  # Redirect to the profile page
+    
+    return render_template('login.html')
+
+@contestmanagement_bp.route("/contest_page")
 def contest_page():
     return render_template("contest.html", contests=contests, user=current_user)
 
-@app.route('/create_contest', methods=['GET', 'POST'])
+@contestmanagement_bp.route('/create_contest', methods=['GET', 'POST'])
 def create_contest():
     if current_user.role != 'admin':  #Check if user is an admin
         flash("Access Denied. Admins Only!", "error")
         return redirect(url_for('home'))  #Send non-admins users to homepage
     return render_template('create_contest.html')
 
+@contestmanagement_bp.route('/submit_contest', methods=['POST'])
 def check_user_submission(user_id):
     existing_entry = Submission.query.filter_by(user_id=user_id).first()
     if existing_entry:
