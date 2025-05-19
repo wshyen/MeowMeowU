852672c37c2075f791ecc0f643052fc604f95126
Yinnie <yinniesiew@gmail.com>
2025-05-20 01:01:09 +0800

    add python file

---
+++ Website/relationship.py
@@ -0,0 +1,11 @@
+import sqlite3
+from flask import Blueprint, g, render_template
+
+relationship_bp = Blueprint('relationship', __name__, template_folder='templates')
+
+def get_db_connection():
+    db_path = os.path.join(os.path.dirname(__file__), '..', 'instance', 'datebase.db')
+    db_path = os.path.abspath(db_path) 
+    conn = sqlite3.connect(db_path)
+    conn.row_factory = sqlite3.Row
+    return conn
